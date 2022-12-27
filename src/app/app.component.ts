95ccae5b4fa34f6578ef538da1b92650ff77cdab
Med louay ben belgacem <mohamedlouay.benbelgacem@esprit.tn>
2022-12-27 12:28:54 +0100

    data ara visual-imageServer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,9 @@ import { ServerService } from './service/server.service';
 })
 export class AppComponent implements OnInit {
   appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+
+
   constructor(private serverService: ServerService) { }
 
 
